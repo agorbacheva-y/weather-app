5e9b8172edb938959451b1ae2a3dcd7471fc04a7
agorbacheva-y <agorbacheva.y@gmail.com>
2023-10-16 21:47:11 +0200

    save response in state

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,46 +1,56 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Fetch() {
+  const [ weather, setWeather ] = useState({});
+
   const APIKey = process.env.REACT_APP_APIKEY;
   let city = "";
 
   useEffect(() => {
     fetchWeather("Tokyo");
-  },[]);
+    console.log(weather);
+  },[weather]);
 
-  const fetchWeather = async (city) => { 
-    try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
+  // fetch API
+  const fetchWeather = (city) => { 
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+    axios
+      .get(url)
+      .then((response) => {
+        setWeather(response.data);
+      })
+      .catch(error => {
+        console.log(error.response.status)
+      });
 
-      let cityName = data.name;
-      let temp = data.main.temp;
-      let weather = data.weather[0].main;
-      console.log(`city: ${cityName}, temp: ${temp}, weather: ${weather}`);
-
-      let sunrise = new Date(data.sys.sunrise * 1000);
-      let sunriseHour = sunrise.getHours().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
-      let sunriseMinutes = sunrise.getMinutes().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
-      let sunset = new Date(data.sys.sunset * 1000);
-      let sunsetHour = sunset.getHours().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
-      let sunsetMinutes = sunset.getMinutes().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
-      console.log(`
-      sunrise: ${sunriseHour}:${sunriseMinutes}
-      sunset: ${sunsetHour}:${sunsetMinutes}
-      `)
-
-    } catch (err) {
-      console.log(err.response.status);
-    }
-
-   
+      // // save sunrise and sunset times
+      // let sunrise = new Date(data.sys.sunrise * 1000);
+      // let sunriseTime = getSunriseTime(sunrise);
+      // let sunset = new Date(data.sys.sunset * 1000);
+      // let sunsetTime = getSunsetTime(sunset);
+      // console.log(sunriseTime, sunsetTime);
   };
+
+  // functions to get sunrise and sunset time
+  const getSunriseTime = (sunrise) => {
+    let sunriseHour = sunrise.getHours().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
+    let sunriseMinutes = sunrise.getMinutes().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
+    return `sunrise: ${sunriseHour}:${sunriseMinutes}`;
+  };
+
+  const getSunsetTime = (sunset) => {
+    let sunsetHour = sunset.getHours().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
+    let sunsetMinutes = sunset.getMinutes().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
+    return `sunset: ${sunsetHour}:${sunsetMinutes}`;
+  };
+
 
   return (
     <div>
-      
+      <p>{weather.main.temp}&deg;C</p>
+      <p>{weather.name}</p>
+      <p>{weather.weather[0].main}</p>
     </div>
   )
 }
