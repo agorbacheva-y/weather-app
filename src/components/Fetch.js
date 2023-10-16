d941bc67f7f46d960fc3d42f78f9c36c9ebb6279
agorbacheva-y <agorbacheva.y@gmail.com>
2023-10-16 17:15:51 +0200

    sunset sunrise times

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,14 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 function Fetch() {
   const APIKey = process.env.REACT_APP_APIKEY;
   let city = "";
-  
 
   useEffect(() => {
-    fetchWeather("London");
+    fetchWeather("Tokyo");
   },[]);
-
 
   const fetchWeather = async (city) => { 
     try {
@@ -16,9 +14,28 @@ function Fetch() {
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
+
+      let cityName = data.name;
+      let temp = data.main.temp;
+      let weather = data.weather[0].main;
+      console.log(`city: ${cityName}, temp: ${temp}, weather: ${weather}`);
+
+      let sunrise = new Date(data.sys.sunrise * 1000);
+      let sunriseHour = sunrise.getHours().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
+      let sunriseMinutes = sunrise.getMinutes().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
+      let sunset = new Date(data.sys.sunset * 1000);
+      let sunsetHour = sunset.getHours().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
+      let sunsetMinutes = sunset.getMinutes().toLocaleString("sv-SE", { minimumIntegerDigits: 2 });
+      console.log(`
+      sunrise: ${sunriseHour}:${sunriseMinutes}
+      sunset: ${sunsetHour}:${sunsetMinutes}
+      `)
+
     } catch (err) {
       console.log(err.response.status);
     }
+
+   
   };
 
   return (
